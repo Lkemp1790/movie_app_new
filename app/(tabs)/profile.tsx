8d1ce6459b0d233b445b8a884cae0c98b02cb04a
Liam Kemp <lkemp1790@gmail.com>
2025-04-10 19:07:07 +0100

    Refactor RootLayout to include tab and movie screens; remove unused index component for cleaner structure.

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+const Profile = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+
+      <Text className="text-2xl text-primary font-bold">Welcome!</Text>
+    </View>
+  );
+}
+
+export default Profile;
+
---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,14 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+const Search = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+
+      <Text className="text-2xl text-primary font-bold">Welcome!</Text>
+    </View>
+  );
+}
+
+export default Search;
+
